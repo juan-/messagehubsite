90a9b433bf527ba66bb65dea6dc8be7ef038b03a
Juan <jvaron@princeton.edu>
2015-09-01 06:14:48 -0400

    First Up

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>JusText</title>
+		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link href="css/slider.css" rel="stylesheet" type="text/css" />
+		<link href="css/main.css" type="text/css" rel="stylesheet" />
+        <link href="css/index.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<div class="nav">
+			
+		</div>
+		<a name="top"></a>
+		<div class="landing">
+			<div class="landing-center">
+				<h1><font color="#f9cb4e">Global</font><font color="#70dbee">Hub</font></h1>
+				<h2>Connect Everything.</h2>
+			</div>
+			<a href="#whatis"><i class="glyphicon glyphicon-chevron-down landing-arrow"></i></a>
+		</div>
+		<a name="whatis"></a>
+		<div class="wrapper">
+			<div class="showcase-section" style="padding:1rem;text-align:center;margin:auto;margin-top:8rem;font-size:1.8rem;line-height:2.8rem;font-family:Roboto;font-weight:300;max-width:800px;margin-bottom:5rem;">
+				
+				<h3>What is GlobalHub?</h3>
+				In a world in which <b>over half</b> of all mobile phones are not "smart" and devices consume a lot of power, GlobalHub aims to bring the benefits of grouped global and regional communication to those without such privileges at a nearly negligible cost. Through the simple <i>Web</i> and <i>SMS</i> system, GlobalHub is able to quickly connect millions of devices instantaneously.
+				<br><br><hr><br><br>
+				<h3> Live Demo Instructions</h3>
+				To join the global chat room text your message to: <b>+1 (609) 557-3056</b> <br>
+				Make sure you joing the web version so that you can see the real-time messages! <br>
+				<b><i>Features:</i></b> text "subscribe" to receive messages posted in the chat room, and text "ubsubscribe" once you are done. <br>
+				<a href="https://globalhub.herokuapp.com" target="_new"><button style="padding:1rem;margin:2rem"> Live Demo </button></a>
+			</div>
+			
+		</div>
+		<div class="footer">
+			<div class="footer-left">
+				&copy; GlobalHub 2015 - <a target="_blank" href="http://www.aboutjuan.com/">Juan Sepulveda</a> - Made for KPCB Fellows Challenge
+			</div>
+		</div>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+		<script src="js/index.js"></script>
+	</body>
+</html>
